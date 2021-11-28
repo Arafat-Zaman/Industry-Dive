d5d9e258e726c60fc25f4066feb1c374565c98f7
Arafat-Zaman <arafatzamananik@gmail.com>
2021-11-28 17:20:51 +0600

    creating file structure

---
+++ webmysite/webmysite/views.py
@@ -0,0 +1,12 @@
+# I have created this file - arafat
+from django.http import HttpResponse
+
+def index(request):
+     return HttpResponse('''<h1>hello arafat</h1> <a href="https://www.youtube.com/">Youtube click on here</a>''')
+
+def about(request):
+    return HttpResponse("<h1>hello arafat</h1>")
+
+def about(request):
+    return HttpResponse("about hello arafat")
+
